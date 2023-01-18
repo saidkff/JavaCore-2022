e238b848c3def5e77660597c568904d4c4be77cd
Zhavokhir Saidkulov <zhavokhir02@gmail.com>
2023-01-18 13:08:10 +0600

    Deleted some files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,7 @@
 package com.company;
 
-import java.util.stream.IntStream;
-
 public class Main {
     public static void main(String[] args) {
-        IntStream.iterate(0, it->it+1)
-                .limit(100)
-                .mapToObj(val->"HelloWorld")
-                .forEach(System.out::println);
+        System.out.println("Hello World");
     }
 }
